58531c863fcfd79740d362972ad04ba6f0db40ce
MrWatchman <joaooli17@hotmail.com>
2023-03-24 22:17:34 -0300

    Alterado o timeout do socket

--- scan_nmap.py
+++ scan_nmap.py
@@ -18,7 +18,7 @@ print("-" * 50)
 try:
     for port in range(1, 65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.5)
+        socket.setdefaulttimeout(1)
 
         #open port
         result = s.connect_ex((target, port))
